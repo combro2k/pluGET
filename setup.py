a236261911088805c327445361df5c0d15370de8
Martijn van Maurik <git@vmaurik.nl>
2021-05-29 16:34:32 +0200

    Add setup.py to install via pip3 and/or pipx

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+from distutils.core import setup
+
+setup(
+    name='pluGET',
+    version='0.1dev',
+    packages=['pluGET',],
+    package_dir={'pluGET': 'src'},
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+    entry_points = {
+        'console_scripts': [
+            'pluGET=pluGET:mainFunction'
+        ],
+    },
+    install_requires=[
+        'urllib3>=1.21.1',
+        'requests>=2.25.1',
+        'paramiko>=2.7.2',
+        'bcrypt>=3.2.0',
+        'cryptography>=3.4.6',
+        'pynacl>=1.4.0',
+        'cffi>=1.14.5',
+        'six>=1.15.0',
+        'pycparser>=2.20',
+        'pysftp>=0.2.9',
+        'rich>=9.13.0',
+        'commonmark>=0.9.1',
+        'Pygments>=2.8.1',
+        'typing_extensions>=3.7.4.3',
+    ],
+)
