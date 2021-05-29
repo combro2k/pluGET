54cb421ff981849f488c7f2fe45d94cd920db62b
Martijn van Maurik <git@vmaurik.nl>
2021-05-29 19:28:41 +0200

    check if the fetched data / url is not 404

--- src/utils/web_request.py
+++ src/utils/web_request.py
@@ -5,5 +5,9 @@ import requests
 def doAPIRequest(url):
     headers = {'user-agent': 'pluGET/1.0'}
     response = requests.get(url, headers=headers)
-    packageDetails = response.json()
+    if response.status_code != 404:
+        packageDetails = response.json()
+    else:
+        packageDetails = None
+
     return packageDetails
